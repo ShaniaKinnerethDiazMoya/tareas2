51d110012979896b82534b05bceba956b1ed023f
Shania Diaz <shania11diaz@gmail.com>
2025-05-20 22:20:50 -0600

    Subiendo tarea

---
+++ ejercicios1.js
@@ -0,0 +1,4 @@
+let num1 = parseFloat(prompt("Ingresa el primer número:"));
+let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+let suma = num1 + num2;
+console.log("La suma es: " + suma);
