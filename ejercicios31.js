c88fb4616ed773346d2577ca9ba4a6e363a3f586
Shania Diaz <shania11diaz@gmail.com>
2025-06-14 22:20:50 -0600

    Subiendo tarea

---
+++ ejercicios31.js
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros = {};
+        
+        if (numeros.length == 0) {
+            System.out.println("El array está vacío.");
+        } else {
+            System.out.println("El array no está vacío.");
+        }
+    }
+}
